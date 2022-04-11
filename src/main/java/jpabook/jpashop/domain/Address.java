ff88d312973e743d0160a93c9e7e799dd358b8f7
조용일 <yedn1128@naver.com>
2022-04-11 19:13:41 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
+// 값 타입은 기본적으로 값이 변경이 되면 안됨.
+// 생성 할 때만 값이 세팅 되는 방식이 좋음
 @Embeddable
 @Getter
 public class Address {
@@ -11,5 +13,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 
 }
---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long Id;
+
+    private String name;
+
+    // 실무에선 Many To Many 쓰면 안됨
+    // 중간 테이블에 필요한 값을 추가로 넣기가 어려움
+    @ManyToMany
+    @JoinTable(
+            name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+}
